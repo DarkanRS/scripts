b1627c7693b73336e6d5b7162c2b6aca9f9c0f2b
Trenton Kress <trenton.kress@gmail.com>
2022-02-08 05:44:48 -0800

    Update setup-lobby.sh

--- setup-lobby.sh
+++ setup-lobby.sh
@@ -5,9 +5,10 @@ mkdir /darkan
 cd /darkan
 mkdir lobby
 cd lobby
-git clone git@github.com:titandino/darkan-lobby-server.git
-git clone git@github.com:titandino/darkan-core.git
-git clone git@github.com:titandino/darkan-cache.git
+git clone git@github.com:DarkanRS/lobby-server.git
+git clone git@github.com:DarkanRS/core.git
+git clone git@github.com:DarkanRS/cache.git
+cd core && ./gradlew publishToMavenLocal
 iptables -A INPUT -p tcp --dport 43594 -j ACCEPT
 iptables -A INPUT -p tcp --dport 43595 -j ACCEPT
 iptables -A INPUT -p tcp --dport 4040 -j ACCEPT
